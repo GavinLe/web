6eba2bce3b0797f9cacff6f86cadc2bb2a2c1a07
Gavin Le <lxq_360@hotmail.com>
2016-06-27 11:18:01 +0800

    we to wb

--- play/settings.py
+++ play/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'we.apps.WbConfig'
+    'wb.apps.WbConfig'
 ]
 
 MIDDLEWARE_CLASSES = [
